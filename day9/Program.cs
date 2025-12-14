85a4b7455044be680ae2d412bb46354432e75dde
Mikkel Bergstrand <mikkel.a.bergstrand@ntnu.no>
2025-12-14 20:04:12 +0100

    I apogize for this solution.

--- day9/Program.cs
+++ day9/Program.cs
@@ -21,10 +21,6 @@ struct Point {
   }
 
   public static bool operator!=(Point a, Point b) { return !(a == b); }
-
-  public override string ToString() {
-    return $"({x},{y})";
-  }
 }
 
 class Tile {
@@ -43,6 +39,12 @@ class Tile {
   }
 }
 
+public struct Wall {
+  public int coordinate; 
+  public int start;
+  public int end;
+}
+
 public class Program {
   static int mod(int x, int m) {
       return (x%m + m)%m;
@@ -50,30 +52,6 @@ public class Program {
 
   static bool inRange(int x, int low, int high) {
     return x > low && x < high;
-  }
-
-  static bool CheckForEdgeConflict(Tile a, Tile b, IEnumerable<Tile> allTiles) {
-    int lbx = Math.Min(a.location.x, b.location.x);
-    int ubx = Math.Max(a.location.x, b.location.x);
-    int lby = Math.Min(a.location.y, b.location.y);
-    int uby = Math.Max(a.location.y, b.location.y);
-
-    // Check if any tiles on the edge has a normal vector
-    // that points away from the shape. This means that the rect constructed by
-    // (a, b)
-    // fills up outside the shape.
-    var tmp =  allTiles.Where(t => {
-        return 
-          t.location != a.location && t.location != b.location && 
-          (inRange(t.location.y, lby, uby) && (t.location.x == lbx && t.normal.x < 0)) ||
-          (inRange(t.location.y, lby, uby) && (t.location.x == ubx && t.normal.x > 0)) ||
-          (inRange(t.location.x, lbx, ubx) && (t.location.y == lby && t.normal.y < 0)) ||
-          (inRange(t.location.x, lbx, ubx) && (t.location.y == uby && t.normal.y > 0));
-    });
-    if (tmp.Count() > 0) {
-      Console.WriteLine($"Trouble at {tmp.First().location} for {a.location} and {b.location}");
-    }
-    return tmp.Any();
   }
 
   public static void Main(string[] args) {
@@ -87,6 +65,10 @@ public class Program {
     // and the exterior of the enclosed shape.
     int leftMostX = int.MaxValue;
     int leftIndex = -1;
+
+    var horizontalWalls = new List<Wall>();
+    var verticalWalls = new List<Wall>();
+
     for(int i = 0; i <= tiles.Length; i++) {
       int prev = i % tiles.Length;
       int next = (i + 1) % tiles.Length;
@@ -94,6 +76,20 @@ public class Program {
       if (tiles[prev].location.x == tiles[next].location.x && tiles[prev].location.x < leftMostX) {
         leftMostX = tiles[prev].location.x;
         leftIndex = i;
+      }
+      
+      if(tiles[prev].location.x == tiles[next].location.x) {
+        verticalWalls.Add(new Wall{
+          coordinate = tiles[prev].location.x,
+          start = Math.Min(tiles[prev].location.y, tiles[next].location.y),
+          end = Math.Max(tiles[prev].location.y, tiles[next].location.y),
+        });
+      } else {
+        horizontalWalls.Add(new Wall{
+          coordinate = tiles[prev].location.y,
+          start = Math.Min(tiles[prev].location.x, tiles[next].location.x),
+          end = Math.Max(tiles[prev].location.x, tiles[next].location.x),
+        });
       }
     }
 
@@ -113,7 +109,6 @@ public class Program {
     for(int i = 0; i < tiles.Count()+1; i++) {
       int prev = (i + leftIndex ) % tiles.Count();
       int next = (i + leftIndex + 1) % tiles.Count();
-
 
       Console.WriteLine(tiles[prev].location);
       bool horiz = tiles[next].location.y == tiles[prev].location.y;
@@ -138,10 +133,6 @@ public class Program {
       prevHoriz = horiz;
     }
 
-    Console.WriteLine("Normals computed.");
-    foreach(Tile t in tiles) {
-      Console.WriteLine($"{t.location}, {t.normal}, {t.restrictive}");
-    }
     long maxArea = 0;
     for(int i = 0; i < tiles.Length; i++) {
       Tile tile = tiles[i];
@@ -180,21 +171,27 @@ public class Program {
       foreach(Tile candidate in candidates) {
         long candidateArea = Tile.Area(tile, candidate);
         if (candidateArea > maxArea) {
-          // Initially check if any tiles intersect, not counting the edge
-          bool intersects = tiles.Where(t => {
-              return t.location.x >= Math.Min(tile.location.x, candidate.location.x)+1 &&
-                t.location.x <= Math.Max(tile.location.x, candidate.location.x)-1 && 
-                t.location.y >= Math.Min(tile.location.y, candidate.location.y)+1 &&
-                t.location.y <= Math.Max(tile.location.y, candidate.location.y)-1;
-              }).Any();
-          if (!intersects) {
-            if (!CheckForEdgeConflict(tile, candidate, tiles)) {
-              Console.WriteLine($"new max: {candidateArea}, with={tile.location} {tile.normal} {tile.restrictive}, {candidate.location} {candidate.normal} {candidate.restrictive}");
-              maxArea = candidateArea;
-            }
-          }       
-        }
 
+          // Initially check if any tiles intersect, not counting the edge
+          int lbx = Math.Min(tile.location.x, candidate.location.x);
+          int ubx = Math.Max(tile.location.x, candidate.location.x);
+          int lby = Math.Min(tile.location.y, candidate.location.y);
+          int uby = Math.Max(tile.location.y, candidate.location.y);
+
+          var horizIntersects = horizontalWalls.Where(w => {
+              return w.coordinate > lby && w.coordinate < uby &&
+                Math.Max(lbx, w.start) < Math.Min(ubx, w.end);
+              });
+
+          var vertIntersects = verticalWalls.Where(w => {
+              return w.coordinate > lbx && w.coordinate < ubx &&
+                Math.Max(lby, w.start) < Math.Min(uby, w.end);
+              });
+
+          if (!horizIntersects.Any() && !vertIntersects.Any()) {
+            maxArea = candidateArea;
+          }
+        }
       }
     }
     Console.WriteLine(maxArea);
